b7c497be0ea12ecef573310bba5e133060ff75f7
varsha-shaji <varshamariyashaji2002@gmail.com>
2026-02-26 10:24:04 +0530

    Calculator program

---
+++ calculator.py
@@ -0,0 +1,27 @@
+import sys
+
+try:
+    a = int(input("Enter first Number :"))
+    b = int(input("Enter Second Number:"))
+    ch = input("Enter a operation (+,-,*,/): ")
+    if ch == "+":
+        res = a + b
+
+    elif ch == "-":
+        res = a - b
+
+    elif ch == "*":
+        res = a * b
+    elif ch == "/":
+        try:
+            res = a / b
+
+        except:
+            print(sys.exc_info())
+            sys.exit(1)
+    else:
+        print("invalid operation")
+    print("{0} {1} {2} is {3}".format(a, ch, b, res))
+
+except:
+    print(sys.exc_info())
